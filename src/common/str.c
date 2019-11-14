990ffc9ee550dd1482cff1fe4e9ee368765d26b0
Positron <positron@rogers.com>
2019-11-14 14:45:17 -0500

    Update string functions

--- src/common/str.c
+++ src/common/str.c
@@ -29,7 +29,7 @@ void str_copy( struct str* str, const char* value, int length ) {
 // If necessary, increases the buffer size enough to fit a string of the
 // specified length and the terminating null character. To select the size, an
 // initial size is doubled until the size is appropriate.
-void adjust_buffer( struct str* string, int length ) {
+static void adjust_buffer( struct str* string, int length ) {
    int required_length = length + 1;
    if ( string->buffer_length < required_length ) {
       int buffer_length = 1;
@@ -65,6 +65,29 @@ void str_append_number( struct str* str, int number ) {
    char buffer[ 11 ];
    sprintf( buffer, "%d", number );
    str_append( str, buffer );
+}
+
+void str_append_format( struct str* str, const char* format, ... ) {
+   va_list args;
+   va_start( args, format );
+   str_append_format_va( str, format, &args );
+   va_end( args );
+}
+
+void str_append_format_va( struct str* str, const char* format,
+   va_list* args ) {
+   va_list args_copy;
+   va_copy( args_copy, *args );
+   int length = vsnprintf( NULL, 0, format, *args );
+   if ( length >= 0 ) {
+      if ( str->length + length >= str->buffer_length ) {
+         str_grow( str, str->length + length + 1 );
+      }
+      vsnprintf( str->value + str->length, str->buffer_length - str->length,
+         format, args_copy );
+      str->length += length;
+   }
+   va_end( args_copy );
 }
 
 void str_clear( struct str* str ) {
